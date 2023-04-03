a1bbfa15798b902aecbed6e8c112b23d8fdea13b
jisanjung <jjjung13@gmail.com>
2023-04-03 16:11:22 -0400

    markup: layout form and prereq info side by side

--- ISRA Dummy Project/Student_RequestForm.aspx.designer.cs
+++ ISRA Dummy Project/Student_RequestForm.aspx.designer.cs
@@ -33,15 +33,6 @@ namespace ISRA_Dummy_Project
         protected global::System.Web.UI.WebControls.Button btnReturn;
 
         /// <summary>
-        /// btnUrp control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnUrp;
-
-        /// <summary>
         /// txtStudentId control.
         /// </summary>
         /// <remarks>
