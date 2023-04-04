fc774145458d506af41228874f10c2dbf64be7a7
Daniel Doan <huydoan2306@gmail.com>
2023-04-04 13:49:53 -0400

    Added Student_Welcome page and content and adding contents to Admin_View

--- ISRA Dummy Project/Student_Welcome.aspx.cs
+++ ISRA Dummy Project/Student_Welcome.aspx.cs
@@ -7,16 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace ISRA_Dummy_Project
 {
-    public partial class Advisor_Admin : System.Web.UI.Page
+    public partial class Student_Welcome : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
-        }
-
-        protected void addbtn_Click(object sender, EventArgs e)
-        {
-            adminAddPanel.visible = true;
+            
         }
     }
 }
